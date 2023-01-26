7bd1d6dea50c4bdfbb2b09113812e433e5549e7b
Gosha Komkov <homosaipios@mail.ru>
2023-01-26 12:55:41 +0300

    Починил тест, отредактировал функцию comparison.js

--- src/comparison.js
+++ src/comparison.js
@@ -7,17 +7,17 @@ const getComparison = (obj1, obj2) => {
   const sortedKeys = _.sortBy(keys);
   const result = sortedKeys.reduce((acc, key) => {
     if (!Object.hasOwn(obj1, key)) {
-      acc.push(`  + ${key}: ${obj2[key]}\n`);
+      acc.push(`  + ${key}: ${obj2[key]}`);
     } else if (!Object.hasOwn(obj2, key)) {
-      acc.push(`  - ${key}: ${obj1[key]}\n`);
+      acc.push(`  - ${key}: ${obj1[key]}`);
     } else if (obj1[key] !== obj2[key]) {
-      acc.push(`  - ${key}: ${obj1[key]} \n  + ${key}: ${obj2[key]}\n`);
+      acc.push(`  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`);
     } else {
-      acc.push(`    ${key}:${obj1[key]}\n`);
+      acc.push(`    ${key}:${obj1[key]}`);
     }
     return acc;
   }, []);
-  return `{\n${result.join('')}}`;
+  return `{\n${result.join('\n')}\n}`;
 };
 
 export default getComparison;
