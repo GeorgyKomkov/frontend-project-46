77ab42deb2d73e55c2b741e959b7aebba7e10075
Gosha-PC <Komkov.g.o@yandex.ru>
2023-02-02 15:21:46 +0300

    Выполнил задания 6-го шага , реализовал нахождение различий для файлов , имеющих вложенные структуры, добавил форматер stylish , разделил форматирование дифа от вычисления различий.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import getComparison from './comparison.js';
+import buildTree from './buildTree.js';
 import getParsedExt from './parses.js';
+import getStylish from './stylish.js';
 
-const getAbsolutePath = (filepath) => path.resolve(path.resolve(filepath));
+const getAbsolutePath = (filepath) => path.resolve(filepath);
 const getData = (absolutePath) => fs.readFileSync(absolutePath);
 const getExtension = (filepath) => path.extname(filepath);
 
@@ -12,11 +13,8 @@ const genDiff = (filepath1, filepath2) => {
   const absolutePathFile2 = getAbsolutePath(filepath2);
   const file1 = getParsedExt(getData(absolutePathFile1), getExtension(absolutePathFile1));
   const file2 = getParsedExt(getData(absolutePathFile2), getExtension(absolutePathFile2));
-  return getComparison(file1, file2);
+  const result = buildTree(file1, file2);
+  return getStylish(result);
 };
-// const genDiff = (filepath1, filepath2) => {
-//   const file1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)));
-//   const file2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)));
-//   return getComparison(file1, file2);
-// };
+
 export default genDiff;
