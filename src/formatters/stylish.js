c4d8b68d9077f587a624f16d34e537c087987f53
Gosha-PC <Komkov.g.o@yandex.ru>
2023-03-01 13:10:07 +0300

    final fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable no-case-declarations */
 import _ from 'lodash';
 
 const indent = (depth, isFull = false) => {
@@ -13,10 +11,10 @@ const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
-  const result = Object.entries(data)
+  const output = Object.entries(data)
     .map(([key, value]) => `${indent(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
 
-  return `${'{'}\n${result.flat().join('\n')}\n${indent(depth)}${'}'}`;
+  return `{\n${output.flat().join('\n')}\n${indent(depth)}}`;
 };
 
 const iter = (tree, depth) => tree.map((node) => {
@@ -27,20 +25,21 @@ const iter = (tree, depth) => tree.map((node) => {
       return `${indent(depth, true)}- ${node.key}: ${stringify(node.value, depth)}`;
     case 'notChanged':
       return `${indent(depth, true)}  ${node.key}: ${stringify(node.value, depth)}`;
-    case 'changed':
+    case 'changed': {
       const output1 = `${indent(depth, true)}- ${node.key}: ${stringify(node.value1, depth)}`;
       const output2 = `${indent(depth, true)}+ ${node.key}: ${stringify(node.value2, depth)}`;
       return `${output1}\n${output2}`;
+    }
     case 'nested':
-      return `${indent(depth)}${node.key}: ${'{'}\n${iter(node.children, depth + 1).flat().join('\n')}\n${indent(depth)}${'}'}`;
+      return `${indent(depth)}${node.key}: {\n${iter(node.children, depth + 1).flat().join('\n')}\n${indent(depth)}}`;
     default:
       throw new Error(`Type: ${node.type} is undefined`);
   }
 });
 
-const getStylish = (diff) => {
-  const stylishDiff = iter(diff, 1);
-  return `${'{'}\n${stylishDiff.flat().join('\n')}\n${'}'}`;
+const getStylish = (output) => {
+  const stylishDiff = iter(output, 1);
+  return `{\n${stylishDiff.flat().join('\n')}\n}`;
 };
 
 export default getStylish;
